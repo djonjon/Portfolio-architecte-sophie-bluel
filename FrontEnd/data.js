156ec79cca6afc3161a3a3351fc122263c81feaf
jon <farrejonathan@yahoo.com>
2023-07-19 12:20:33 +0200

    fetch + modif index .gallery

---
+++ FrontEnd/data.js
@@ -0,0 +1,17 @@
+const gallery = document.querySelector('.gallery');
+    Element.parent
+
+async function fetchWork (){
+    const r = await fetch("http://localhost:5678/api/works", {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+        }
+    })
+    if (r.ok === true) {
+        return r.json();
+    }
+    throw new Error ('Impossible de contacter le serveur')
+}
+
+fetchWork().then(work => console.log(work))
