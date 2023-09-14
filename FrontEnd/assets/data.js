32dcb0f59a86f7a1c9c9b081c5519c6f737dfd96
jon <farrejonathan@yahoo.com>
2023-09-14 18:21:33 +0200

    suppresion photo corbeille

--- FrontEnd/assets/data.js
+++ FrontEnd/assets/data.js
@@ -84,26 +84,51 @@ const modal = document.querySelector('.modal')
 const modalContent = document.getElementById('modal-content')
 const fermerModalBtn = document.getElementById('fermer-modal')
 const btnAddPict = document.getElementById('btn-add-pict')
+//let focusableElements
 
 function openModal(){
     modal.style.display = 'flex'
     document.body.style.overflow = 'hidden'
-    // const focusableElement = modal.querySelectorAll('button, href')
-    // if (focusableElement.length > 0) {
-    //     focusableElement[0].focus()
-    // }
+    modal.addEventListener('click', clickOutModal)
+    //focusableElements = modal.querySelectorAll('button, input, a, select,textarea, figure i')
+    //focusableElements[0].focus()
+
+    //modal.addEventListener('keydown', gestionFocusModal)
+}
+
+// function gestionFocusModal(e){
+//     if (e.key === 'tab') {
+//         if (e.shiftKey) {
+//             if (document.activeElement === focusableElements[0]) {
+//                 e.preventDefault()
+//                 focusableElements[focusableElements.lenght - 1].focus
+//             }
+//         } else {
+//             if (document.activeElement === focusableElements[focusableElements.lenght - 1]){
+//                 e.preventDefault()
+//                 focusableElements[0].focus()
+//             }
+//         }
+//     }
+
+// }
+
+function clickOutModal(e){
+    if (e.target === modal) {
+        fermerModal();
+        modal.removeEventListener('click', clickOutModal)
+    }
 }
 
 function fermerModal(){
     modal.style.display = 'none'
     document.body.style.overflow = 'auto'
+    //modal.removeEventListener('keydown', gestionFocusModal)
 }
 
 function modalAddPict() {
     const titleModal = document.getElementById('title-modal')
     titleModal.textContent = 'Ajout photo'
-
-
 }
 
 document.getElementById('edit').addEventListener('click', openModal);
@@ -116,18 +141,53 @@ function galleryModal(modalData) {
     galleryModal.innerHTML ='';
 
     modalData.forEach( item => {
-        const figure = document.createElement('figure');
-        const img = document.createElement('img');
-        //const poubelle = document.createElement('i')
+        const figure = document.createElement('figure')
+        const img = document.createElement('img')
+        const poubelle = document.createElement('i')
 
-        img.src = item.imageUrl;
-        img.alt = item.title;
-        //poubelle.classList.add('fa-solid', 'fa-trash-can')
+        img.src = item.imageUrl
+        img.alt = item.title
+        poubelle.classList.add('fa-solid', 'fa-trash-can')
 
+        // Récupération de l'ID de la photo et attribution de l'ID à la corbeille
+        const photoId = item.id
+        poubelle.setAttribute('data-photo-id', photoId)
+
+        // Gestionnaire d'événements au clic sur la corbeille 
+        poubelle.addEventListener('click', () => {
+            const confirmation = window.confirm('Êtes-vous sûr de vouloir cette photo ?')
+            if (confirmation) {
+                deletePhoto(photoId)
+            }
+        })
         figure.appendChild(img);
-        galleryModal.appendChild(figure);
-        //figure.appendChild(poubelle)
+        galleryModal.appendChild(figure)
+        figure.appendChild(poubelle)
     });
+}
+
+// function suppression photo
+
+async function deletePhoto(photoId) {
+    try {
+        const response = await fetch(`http://localhost:5678/api/works/${photoId}`, {
+            method: 'DELETE',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': `Bearer ${storedToken}`
+            }
+
+        })
+
+        if (response.ok) {
+            // Photo supprimée et mise à jour de la gallery
+            picturesModal()
+        } else {
+            console.error("La suppression de la photo a échoué")
+        }
+    } catch (error) {
+        console.error("Erreur lors de la suppression de la photo", error)
+    }
 }
 
 // function récupération et affichage des photos dans la modal
